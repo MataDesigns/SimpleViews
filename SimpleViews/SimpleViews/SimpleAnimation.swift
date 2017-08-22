83309ddb3a5bca606c8f6d9776f4838c5f31c2ea
Nicholas Mata <nicholas@matadesigns.net>
2017-08-22 14:42:30 -0700

    Added SimpleCollectionView shares to much code with SimpleTableView. Will need to make a protocol so that duplicate code is down to a minimum.
    Moved animation functions so that they are an extensions on UIVIew

--- SimpleViews/SimpleViews/SimpleAnimation.swift
+++ SimpleViews/SimpleViews/SimpleAnimation.swift
@@ -15,3 +15,115 @@ import UIKit
 public enum SimpleAnimationState {
     case `in`, out
 }
+
+extension UIView {
+    /// Animates a view using center.
+    ///
+    /// - Parameters:
+    ///   - view: The view you would like to animate.
+    ///   - duration: The duration of the animation.
+    ///   - fromCenter: The point you want to animate the view from.
+    ///   - toCenter: The point you want to animate the view to
+    ///   - options: Animation options like curveEaseIn
+    ///   - isIn: Whether the animation is a in or out. Basically at end of animation should the view be hidden or displayed.
+    public func animate(for duration: TimeInterval, fromCenter: CGPoint, toCenter: CGPoint, options: UIViewAnimationOptions, isIn: Bool) {
+        let originalCenter = self.center;
+        self.center = fromCenter
+        self.alpha = 1
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: options, animations: {
+            self.center = toCenter
+        }, completion: { (completed) in
+            self.isHidden = !isIn
+            self.center = originalCenter
+        })
+    }
+    
+    
+    func perform(animation: SimpleAnimation, withState state: SimpleAnimationState) {
+        
+        // Since we are animating this view make sure it
+        // is NOT hidden, or user won't be able to view animation.
+        self.isHidden = false
+        
+        // Whether the transition is a in or out.
+        let isIn = state == .in
+        // TODO: Possibly allow this to be set from delegate.
+        let animationOptions = (isIn ? UIViewAnimationOptions.curveEaseIn : UIViewAnimationOptions.curveEaseOut)
+        // TODO: Possibly allow duration to be set from delegate. Allowing for different duration each time.
+        let duration = 0.7
+        // Screen Dimensions
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        // Original center of the view is used in calculating animate.
+        let originalCenter = self.center;
+        
+        // The points to animate center of the view from and to.
+        var _from: CGPoint?
+        var _to: CGPoint?
+        
+        // Select the appropiate transition.
+        switch animation {
+        case .fade:
+            // Make the view appear and disappear aka change alpha
+            UIView.animate(withDuration: duration, delay: 0.0, options: animationOptions, animations: {
+                self.alpha = isIn ? 1 : 0
+            }, completion: { (completed) in
+                self.isHidden = !isIn
+            })
+        case .leftToRight:
+            // Make view animate coming in from the left of the screen and exiting to the right.
+            _from = isIn ? CGPoint(x: -self.bounds.width, y: originalCenter.y) : originalCenter
+            _to   = isIn ? originalCenter : CGPoint(x: screenWidth+self.bounds.width, y: originalCenter.y)
+        case .rightToLeft:
+            // Make view animate coming in from the right of the screen and exiting to the left.
+            _from = isIn ? CGPoint(x: screenWidth+self.bounds.width, y: originalCenter.y) : originalCenter
+            _to   = isIn ? originalCenter : CGPoint(x: -self.bounds.width, y: originalCenter.y)
+        case .slideUp:
+            // Make view animate coming in from the bottom of the screen and exiting to the top.
+            _from = isIn ? CGPoint(x: originalCenter.x, y: screenHeight+self.center.y) : originalCenter
+            _to   = isIn ? originalCenter : CGPoint(x: originalCenter.x, y: -self.bounds.height)
+        case .slideDown:
+            // Make view animate coming in from the top of the screen and exiting to the bottom.
+            _from = isIn ? CGPoint(x: originalCenter.x, y: -self.bounds.height) : originalCenter
+            _to   = isIn ? originalCenter : CGPoint(x: originalCenter.x, y: screenHeight+self.center.y)
+        case .none:
+            // No animation just simple display or hide view.
+            self.isHidden = !isIn
+        }
+        
+        // Make sure we have both a _from and _to since required to animate.
+        guard let from = _from, let to = _to else {
+            return
+        }
+        
+        // Animate the view.
+        self.animate(for: duration, fromCenter: from, toCenter: to, options: animationOptions, isIn: isIn)
+    }
+}
+
+//public class SimpleAnimation {
+//    
+//    /// Animates a view using center.
+//    ///
+//    /// - Parameters:
+//    ///   - view: The view you would like to animate.
+//    ///   - duration: The duration of the animation.
+//    ///   - fromCenter: The point you want to animate the view from.
+//    ///   - toCenter: The point you want to animate the view to
+//    ///   - options: Animation options like curveEaseIn
+//    ///   - isIn: Whether the animation is a in or out. Basically at end of animation should the view be hidden or displayed.
+//    public static func animate(_ view: UIView, for duration: TimeInterval, fromCenter: CGPoint, toCenter: CGPoint, options: UIViewAnimationOptions, isIn: Bool) {
+//        let originalCenter = view.center;
+//        view.center = fromCenter
+//        view.alpha = 1
+//        
+//        UIView.animate(withDuration: duration, delay: 0.0, options: options, animations: {
+//            view.center = toCenter
+//        }, completion: { (completed) in
+//            view.isHidden = !isIn
+//            view.center = originalCenter
+//        })
+//    }
+//    
+//}
--- SimpleViews/SimpleViews/SimpleCollectionView.swift
+++ SimpleViews/SimpleViews/SimpleCollectionView.swift
@@ -1,57 +1,57 @@
 //
-//  SimpleTableView.swift
+//  SimpleCollectionView.swift
 //  SimpleViews
 //
-//  Created by Nicholas Mata on 7/29/17.
+//  Created by Nicholas Mata on 8/22/17.
 //  Copyright © 2017 MataDesigns. All rights reserved.
 //
 
 import UIKit
 
-@objc public protocol SimpleTableViewDelegate {
+@objc public protocol SimpleCollectionViewDelegate {
     
     /// The view that should be displayed for the state of the SimpleTableView.
     ///
     /// - Parameter:
-    ///   - simpleTableView: The SimpleTableView object requesting this information.
+    ///   - simpleCollectionView: The SimpleTableView object requesting this information.
     ///   - state: The state for which we would like the view for.
     /// - Returns: The view that should be displayed when in this state.
-    func simpleTableView(_ simpleTableView: SimpleTableView, viewFor state: SimpleViewState) -> UIView
+    func simpleCollectionView(_ simpleCollectionView: SimpleCollectionView, viewFor state: SimpleViewState) -> UIView
     
     /// The animation for given state.
     ///
     /// - Parameter:
-    ///   - simpleTableView: The SimpleTableView object requesting this information.
+    ///   - simpleCollectionView: The SimpleTableView object requesting this information.
     ///   - state: The state for which we would like the animation for.
     /// - Returns: The animation that should be performed when view is entering or leaving this state.
-    func simpleTableView(_ simpleTableView: SimpleTableView, animationFor state: SimpleViewState) -> SimpleAnimation
+    func simpleCollectionView(_ simpleCollectionView: SimpleCollectionView, animationFor state: SimpleViewState) -> SimpleAnimation
     
     /// Optional: The out animation when leaving a specific state. If function not implemented default out animation for
     /// simpleTableView(_ simpleTableView: SimpleTableView, animationFor state: SimpleViewState) -> SimpleAnimation will be used.
     ///
     /// - Parameters:
-    ///   - simpleTableView: The SimpleTableView object requesting this information.
+    ///   - simpleCollectionView: The SimpleTableView object requesting this information.
     ///   - state: The state for which we would like the out animation for.
     /// - Returns: The out animation that should be performed when view is leaving this state.
-    @objc optional func simpleTableView(_ simpleTableView: SimpleTableView, outAnimationFor state: SimpleViewState) -> SimpleAnimation
+    @objc optional func simpleCollectionView(_ simpleCollectionView: SimpleCollectionView, outAnimationFor state: SimpleViewState) -> SimpleAnimation
     
     /// Optional: Notification for whenever SimpleTableView state is changed.
     ///
     /// - Parameters:
-    ///   - simpleTableView: The SimpleTableView that is notifying the change.
+    ///   - simpleCollectionView: The SimpleTableView that is notifying the change.
     ///   - oldState: The old state of the SimpleTableView.
     ///   - state: The new state of the SimpleTableView.
-    @objc optional func simpleTableView(_ simpleTableView: SimpleTableView, stateChangedFrom oldState: SimpleViewState, to state: SimpleViewState)
+    @objc optional func simpleCollectionView(_ simpleCollectionView: SimpleCollectionView, stateChangedFrom oldState: SimpleViewState, to state: SimpleViewState)
 }
 
-public class SimpleTableView: UITableView {
+public class SimpleCollectionView: UICollectionView {
     
     /// Current state of the TableView.
     public var state: SimpleViewState = .loading {
         didSet {
             DispatchQueue.global(qos: .background).sync {
                 // Notify delegate that the state did change.
-                self.simpleDelegate?.simpleTableView?(self, stateChangedFrom: oldValue, to: state)
+                self.simpleDelegate?.simpleCollectionView?(self, stateChangedFrom: oldValue, to: state)
             }
             
             // In order to perform animations simpleDelegate must be set.
@@ -59,21 +59,21 @@ public class SimpleTableView: UITableView {
                 return
             }
             // Get view for previous state.
-            let previousView = simpleDelegate.simpleTableView(self, viewFor: oldValue)
+            let previousView = simpleDelegate.simpleCollectionView(self, viewFor: oldValue)
             // Get out animation for previous state.
-            var outAnimation = simpleDelegate.simpleTableView?(self, outAnimationFor: oldValue)
+            var outAnimation = simpleDelegate.simpleCollectionView?(self, outAnimationFor: oldValue)
             if (outAnimation == nil) {
                 // Get out animation for previous state based on in animation.
-                outAnimation = simpleDelegate.simpleTableView(self, animationFor: oldValue)
+                outAnimation = simpleDelegate.simpleCollectionView(self, animationFor: oldValue)
             }
             // Get view for current state.
-            let view = simpleDelegate.simpleTableView(self, viewFor: state)
+            let view = simpleDelegate.simpleCollectionView(self, viewFor: state)
             // Only perform animation if views are different.
             if previousView != view {
                 // Perform out animation for previous state.
                 previousView.perform(animation: outAnimation!, withState: .out)
                 // Get in animation for current state.
-                let inAnimation = simpleDelegate.simpleTableView(self, animationFor: state)
+                let inAnimation = simpleDelegate.simpleCollectionView(self, animationFor: state)
                 // Perform in animation for current state.
                 view.perform(animation: inAnimation, withState: .in)
             }
@@ -84,19 +84,20 @@ public class SimpleTableView: UITableView {
     public var fetched: Bool = false
     
     /// Delegate used for activiting states.
-    public var simpleDelegate: SimpleTableViewDelegate?
+    public var simpleDelegate: SimpleCollectionViewDelegate?
     
     /// The total number of rows in all sections of the TableView.
-    private var totalRows: Int {
+    private var totalItems: Int {
         get{
-            var totalRows = 0
+            var totalItems = 0
             for i in 0 ..< (self.dataSource?.numberOfSections?(in: self) ?? 0) {
-                let rowsInSection = self.dataSource?.tableView(self, numberOfRowsInSection: i) ?? 0
-                totalRows += rowsInSection
+                let itemsInSection = self.dataSource?.collectionView(self, numberOfItemsInSection: i) ?? 0
+                totalItems += itemsInSection
             }
-            return totalRows
+            return totalItems
         }
     }
+
     
     /// Will hide views but the view for the current state.
     /// simpleDelegate must be set before calling this or nothing will happen.
@@ -108,14 +109,14 @@ public class SimpleTableView: UITableView {
         
         for state in iterateEnum(SimpleViewState.self) {
             if(state != self.state) {
-                let view = simpleDelegate.simpleTableView(self, viewFor: state)
+                let view = simpleDelegate.simpleCollectionView(self, viewFor: state)
                 initialize(view: view)
             }
         }
         
-        let initialView = simpleDelegate.simpleTableView(self, viewFor: state)
+        let initialView = simpleDelegate.simpleCollectionView(self, viewFor: state)
         initialView.alpha = 0.0
-        let initialAnimation = simpleDelegate.simpleTableView(self, animationFor: state)
+        let initialAnimation = simpleDelegate.simpleCollectionView(self, animationFor: state)
         initialView.perform(animation: initialAnimation, withState: .in)
     }
     
@@ -138,7 +139,7 @@ public class SimpleTableView: UITableView {
         if state != .failed {
             if fetched {
                 // Set states based off row count.
-                switch totalRows {
+                switch totalItems {
                 case let rows where rows > 1:
                     state = .finished
                 case 0:
@@ -153,4 +154,13 @@ public class SimpleTableView: UITableView {
         
         super.reloadData()
     }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
 }
